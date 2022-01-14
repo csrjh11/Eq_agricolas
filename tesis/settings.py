5a4f3fc78e0f6945345c820809ad8dc0304019e8
César <cesarjacobohdz@gmail.com>
2022-01-14 07:43:29 -0600

    int

--- tesis/settings.py
+++ tesis/settings.py
@@ -81,6 +81,16 @@ WSGI_APPLICATION = 'tesis.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "datos_tesis",
+#         "USER" : "postgres",
+#         "PASSWORD" : "Jac0060*",
+#         "HOST" : "localhost",
+#         "PORT" : "5432",
+# }
+
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
