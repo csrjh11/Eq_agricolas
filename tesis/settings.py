44f34dbc269e80b958b4871a46ec04a542d38ca6
César <cesarjacobohdz@gmail.com>
2022-01-28 15:32:43 -0600

    modif Settings

--- tesis/settings.py
+++ tesis/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = (
